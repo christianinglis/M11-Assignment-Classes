ccf5798e464a450b16a6f1be1711addf0d0e7822
Christian Inglis <ccinglis@my.waketech.edu>
2026-04-07 13:22:57 -0400

    Test your implemation

--- movies.js
+++ movies.js
@@ -17,20 +17,30 @@ class Movie {
   updateRating(newRating) {
     this.#rating = newRating;
   }
-}
 
 // Method to display the movie's information
-displayInfo() {
-  const movieInfoDiv = document.getElementById("movie-info");xsaxaswddqw
-  movieInfoDiv.innerHTML = `
-  <h2>Title: ${this.#title}</h2> 
-  <p><strong>Cast:</strong> ${this.#description}</p> 
-  <p><strong>Rating:</strong> ${this.#rating}</p>
+  displayInfo() {
+    const movieInfoDiv = document.getElementById("movie-info");xsaxaswddqw
+    movieInfoDiv.innerHTML = `
+    <h2>Title: ${this.#title}</h2> 
+    <p><strong>Cast:</strong> ${this.#description}</p> 
+    <p><strong>Rating:</strong> ${this.#rating}</p>
   `;
- }
+  }
+
+  getTitle() {
+    return this.#title;
+  }
+}
 
 
 // Test your implementation
+const firstMovie = new Movie(
+  "The Shawshank Redemption",
+  ["Tim Robbins", "Morgan Freeman"],
+  "Two imprisoned men bond over a number of years, finding eventual redemption through acts of common decency",
+  9.3
+);
 
 // Display the initial information of the movie
 
