096c5493e846d55ff251a05c555ea82e603fc2a9
Christian Inglis <ccinglis@my.waketech.edu>
2026-04-07 13:11:44 -0400

    displayInfo

--- movies.js
+++ movies.js
@@ -5,23 +5,29 @@ class Movie {
   #description;
   #rating;
 // title, cast, description, rating
+
   constructor(title, cast, description, rating) {
     this.#title = title;
     this.#cast = cast;
     this.#description = description;
     this.#rating = rating;
   }
+
 // Method to update the rating of the movie
   updateRating(newRating) {
     this.#rating = newRating;
   }
 }
 
-
-
-
-
-  // Method to display the movie's information
+// Method to display the movie's information
+displayInfo() {
+  const movieInfoDiv = document.getElementById("movie-info");xsaxaswddqw
+  movieInfoDiv.innerHTML = `
+  <h2>Title: ${this.#title}</h2> 
+  <p><strong>Cast:</strong> ${this.#description}</p> 
+  <p><strong>Rating:</strong> ${this.#rating}</p>
+  `;
+ }
 
 
 // Test your implementation
