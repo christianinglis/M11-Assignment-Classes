04a24a235b792db9b7e002badea9fa731d2217e8
Christian Inglis <ccinglis@my.waketech.edu>
2026-04-07 13:35:55 -0400

    fix display

--- movies.js
+++ movies.js
@@ -23,7 +23,8 @@ class Movie {
     const movieInfoDiv = document.getElementById("movie-info");
     movieInfoDiv.innerHTML = `
     <h2>Title: ${this.#title}</h2> 
-    <p><strong>Cast:</strong> ${this.#description}</p> 
+    <p><strong>Cast:</strong> ${this.#cast}</p>
+    <p><strong>Description:</strong> ${this.#description}</p>  
     <p><strong>Rating:</strong> ${this.#rating}</p>
   `;
   }
@@ -37,7 +38,7 @@ class Movie {
 // Test your implementation
 const firstMovie = new Movie(
   "The Shawshank Redemption",
-  ["Tim Robbins", "Morgan Freeman"],
+  "Tim Robbins, Morgan Freeman",
   "Two imprisoned men bond over a number of years, finding eventual redemption through acts of common decency",
   9.3
 );
